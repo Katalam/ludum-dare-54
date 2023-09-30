39827473e5090379501e9f4ddcadf54e21580f1e
Katalam <bruno.goerss@hyperlink.eu>
2023-10-01 00:04:30 +0200

    feat: colorize the scheduler

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -5,10 +5,10 @@ export type TimeReachedListener = (destination: Destination) => void;
 
 export class Scheduler extends Graphics {
 
-    public static readonly WIDTH: number = 240;
-    private static readonly HEIGHT: number = 120;
+    public static readonly WIDTH: number = 280;
+    private static readonly HEIGHT: number = 130;
     private static readonly OFFSET_Y: number = 10;
-    private static readonly RECTANGLE_COLOR: number = 0x000000;
+    private static readonly RECTANGLE_COLOR: number = 0x1A00BD;
 
     public static readonly MIN_VALUE_NEXT_TIME: number = 10;
     public static readonly ADD_VALUE_NEXT_TIME: number = 10;
@@ -20,8 +20,10 @@ export class Scheduler extends Graphics {
         super();
 
         this.clear();
+        this.beginFill(0x000000);
+        this.drawRoundedRect(-2, -2, Scheduler.WIDTH + 4, Scheduler.HEIGHT + 5, 10);
         this.beginFill(Scheduler.RECTANGLE_COLOR);
-        this.drawRect(0, 0, Scheduler.WIDTH, Scheduler.HEIGHT);
+        this.drawRoundedRect(0, 0, Scheduler.WIDTH, Scheduler.HEIGHT, 10);
         this.endFill();
 
         this.update(0.0);
@@ -70,7 +72,7 @@ export class Scheduler extends Graphics {
 
 class Entry extends Graphics {
 
-    public static readonly HEIGHT = 30;
+    public static readonly HEIGHT = 18;
 
     private destination: Destination;
     private time: number;
@@ -83,25 +85,46 @@ class Entry extends Graphics {
         this.destination = destination;
 
         this.beginFill(this.destination.getColor());
-        this.drawRect(0, 0, Scheduler.WIDTH, Entry.HEIGHT);
+        this.drawRect(Scheduler.WIDTH / 4 + 40, Entry.HEIGHT / 3, Scheduler.WIDTH / 7, Entry.HEIGHT);
+        this.beginFill(0xffffff);
+        this.drawRect(Scheduler.WIDTH / 3, Entry.HEIGHT / 3, 4, Entry.HEIGHT);
+        this.drawRect(Scheduler.WIDTH / 4 * 2 + 20, Entry.HEIGHT / 3, 4, Entry.HEIGHT);
+        this.drawRect(Scheduler.WIDTH / 4 * 3 + 20, Entry.HEIGHT / 3, 4, Entry.HEIGHT);
         this.endFill();
 
         this.textTime = new Text(this.getTextForClock(), {
             fontFamily: "Arial",
-            fontSize: 25,
-            fill: 0x000000,
+            fontSize: Entry.HEIGHT,
+            fill: 0xffffff,
             align: "center",
         });
         this.addChild(this.textTime)
         this.textTime.x = Scheduler.WIDTH - this.textTime.width - 10;
+        this.textTime.y = 4;
 
-        const textDestination = new Text(this.destination.getName());
+        const textDestination = new Text(this.destination.getName(), {
+            fontFamily: "Arial",
+            fontSize: Entry.HEIGHT,
+            fill: 0xffffff,
+            align: "center",
+        });
         this.addChild(textDestination);
         textDestination.x = 10;
+        textDestination.y = 4;
+
+        const textDestinationShort = new Text(this.destination.destination, {
+            fontFamily: "Arial",
+            fontSize: Entry.HEIGHT,
+            fill: 0xffffff,
+            align: "center",
+        });
+        this.addChild(textDestinationShort);
+        textDestinationShort.x = 175;
+        textDestinationShort.y = 4;
     }
 
     private getTextForClock(): string {
-        return this.time.toFixed(0).toString();
+        return this.time.toFixed(0).toString().padStart(2, '0');
     }
 
     public update(secondsPast: number): void {
