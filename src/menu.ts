41fa862f0b2f7bbb9b217976817bd239f76dc5a2
Katalam <bruno.goerss@hyperlink.eu>
2023-10-01 10:56:51 +0200

    fix: change design for menu buttons

--- src/menu.ts
+++ src/menu.ts
@@ -2,6 +2,7 @@ import { Container, TextStyle, Graphics, Text } from "pixi.js";
 import { app, gameState } from "./index";
 
 export class Menu extends Container {
+    private static readonly PRIMARY_BLUE = 0x1A00BD;
 
     private static TITLE_STYLE = new TextStyle({
         fontSize: 128,
@@ -16,11 +17,8 @@ export class Menu extends Container {
 
     private static BUTTON_STYLE = new TextStyle({
         fontSize: 86,
-        fontWeight: 'bold',
+        fontFamily: "Courier New",
         fill: '#ffffff',
-        stroke: '#000000',
-        strokeThickness: 6,
-        lineJoin: 'round',
     });
 
     private static TEXT_STYLE = new TextStyle({
@@ -152,27 +150,31 @@ export class Menu extends Container {
         // Start Game - Button
         this.startButton.clear();
 
-        this.startButton.lineStyle(5, 0x0707e2);
+        this.startButton.lineStyle(5, Menu.PRIMARY_BLUE);
         if (this.startButtonHover) {
-            this.startButton.beginFill(0x00FFFF, 0.5);
+            this.startButton.beginFill(Menu.PRIMARY_BLUE, 0.5);
         } else {
-            this.startButton.beginFill(0, 0.5);
+            this.startButton.beginFill(Menu.PRIMARY_BLUE, 1.0);
         }
 
-        this.startButton.drawRoundedRect((app.screen.width - 540) / 2, this.startText.y - 20, 540, this.startText.height + 40, 30);
+        this.startButton.drawRoundedRect((app.screen.width - this.startText.width) / 2 - 10, this.startText.y - 20, this.startText.width + 20, this.startText.height + 40, 30);
         this.startButton.endFill();
 
         // How to Play - Button
         this.howToButton.clear();
 
-        this.howToButton.lineStyle(5, 0x0707e2);
+        this.howToButton.lineStyle(5, Menu.PRIMARY_BLUE);
         if (this.howToButtonHover) {
-            this.howToButton.beginFill(0x00FFFF, 0.5);
+            this.howToButton.beginFill(Menu.PRIMARY_BLUE, 0.5);
         } else {
-            this.howToButton.beginFill(0, 0.5);
+            this.howToButton.beginFill(Menu.PRIMARY_BLUE, 1.0);
         }
 
-        this.howToButton.drawRoundedRect((app.screen.width - 540) / 2, this.howToText.y - 20, 540, this.howToText.height + 40, 30);
+        this.howToButton.drawRoundedRect(
+            (app.screen.width - this.howToText.width) / 2 - 10, this.howToText.y - 20,
+            this.howToText.width + 20, this.howToText.height + 40,
+            30
+        );
         this.howToButton.endFill();
     }
 }
